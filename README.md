25c1370fa5b0bdd48a8151a6ea9e3e19e184db24
mamta925 <mamtarajput925@gmail.com>
2023-11-08 22:48:56 +0530

    Agoda leet code

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ npx parcel build index.html
 
 https://parceljs.org/docs/ 
 https://browserslist.dev/?q=bGFzdCAyIHZlcnNpb25z
+https://babeljs.io/
+cross side sripting 
---
+++ src/comonents/Body.js
@@ -0,0 +1,16 @@
+import { resList } from "../utils/mockData";
+import { RestaurantCard } from "./RestaurantCard";
+
+  
+export const Body = () => {
+    return (
+      <div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+          {resList.map((restaurant) => (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          ))}
+        </div>
+      </div>
+    );
+  };
